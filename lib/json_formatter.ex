4cdf006ba1733ad3e54d3d6145ff845d8a359190
jordan-christensen <jkchristensen36@gmail.com>
2025-03-20 11:17:02 -0700

    Initial commit

---
+++ lib/json_formatter.ex
@@ -0,0 +1,18 @@
+defmodule JsonFormatter do
+  @moduledoc """
+  Documentation for `JsonFormatter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JsonFormatter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
